8b8082ac9390cd5969fe7e19006baf0d6c9d9538
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-04-01 09:31:29 -0300

    crontroller para as viagens criadas

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Travel.create(
+    nome: Faker::Travel::Airport.name(size: 'large', region: 'united_states'),
+    desc: Faker::TvShows::BrooklynNineNine.character,
+    data: Faker::Date.between(from: 2.days.ago, to: Date.today),
+    price: Faker::Number.normal(mean: 50, standard_deviation: 3.5),
+)
+
+puts Travel.last
