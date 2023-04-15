9cbec36c6bd762ee82bd7179b22664c02340beea
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-04-15 09:38:39 -0300

    use state

--- frontend/src/exemplos/Geral.js
+++ frontend/src/exemplos/Geral.js
@@ -1,11 +1,16 @@
 import FunctionSimples from "./components/FunctionSimples";
 import FunctionDupla from "./components/FunctionDupla";
 import Botao from "./layout/Botao";
+import {useState} from 'react';
 function Geral(){
+    const [variavel, FunctionMudarVariavel] = useState(false)
     return(
         <>
             <FunctionSimples/>
             <FunctionDupla/>
+            {
+                variavel ? <p>logado</p> : <p>Deslogado</p>
+            }
             <Botao classe="botao purple"> Entrar </Botao>
             <Botao classe="botao blue"> Sair </Botao>
         </>
