be9f7d5cd9e274141f557618406d97ed1361a2f8
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-04-29 09:05:39 -0300

    busca an api ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-import Geral from './exemplos/Geral';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 function App() {
+  const [travels,setTravels] = useState([]);
+  useEffect(()=>{
+    axios.get('http://localhost:3001/api/v1/travels')
+      .then(resposta=>setTravels(resposta.data))
+      .catch(erro=>console.error(erro))
+  })
   return (
     <div className="App">
-      <Geral/>
+      {
+        travels.map(viagem=><h1 key={viagem.id}>{viagem.nome}</h1>)
+      }
     </div>
   );
 }
