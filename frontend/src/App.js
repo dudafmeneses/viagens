c49fb1f191378d94491f015b201284423d1e841f
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-05-06 08:42:05 -0300

    finalizando os campos do formulário

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,9 +34,24 @@ function App() {
         <textarea
           required
           id = "desc"
-          onChange = {(e)=>setTravel({...travel,desc: e,target,value})}
+          onChange = {(e)=>setTravel({...travel,desc: e.target.value})}
           value={travel.desc}
           placeholder = 'Descrição da viagem'
+        />
+        <input
+          requiredtype = 'datetime-local'
+          id = "data"
+          value={travel.data}
+          placeholder = 'Data'
+          onChange = {(e)=>setTravel({...travel,data: e.target.value})}
+        />
+        <input
+          required
+          id = 'price'
+          value = {travel.price}
+          placeholder = 'Preço'
+          type = 'number'
+          onChange = {(e)=>setTravel({...travel,price: e.target.value})}
         />
         <button>Cadastrar Viagem</button>
       </form>
