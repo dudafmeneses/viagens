1a2a1f5781a3f0eb0ac1de8e825bbae45e9b779f
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-05-06 09:11:40 -0300

    css do form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,42 +1,47 @@
+import "./Form.css"
+
 function Form (EnvioFormulario, travel,setTravel){
     return(
         <>
-        <form onSubmit={EnvioFormulario}>
-        <label >Nome</label>
-        <input
-          id ='nome'
-          required
-          value = {travel.nome}
-          placeholder= 'Nome da viagem'
-          onChange = {(e)=>setTravel({...travel,nome: e.target.value})}
-        />
-        <label >Descrição</label>
-        <textarea
-          required
-          id = "desc"
-          onChange = {(e)=>setTravel({...travel,desc: e.target.value})}
-          value={travel.desc}
-          placeholder = 'Descrição da viagem'
-        />
-        <label >Data da viagem</label>
-        <input
-          requiredtype = 'datetime-local'
-          id = "data"
-          value={travel.data}
-          placeholder = 'Data'
-          onChange = {(e)=>setTravel({...travel,data: e.target.value})}
-        />
-        <label >Preço</label>
-        <input
-          required
-          id = 'price'
-          value = {travel.price}
-          placeholder = 'Preço'
-          type = 'number'
-          onChange = {(e)=>setTravel({...travel,price: e.target.value})}
-        />
-        <button>Cadastrar Viagem</button>
-        </form>
+            <div className='divForm'>
+                <form onSubmit={EnvioFormulario}>
+                    <label >Nome</label>
+                    <input
+                    id ='nome'
+                    required
+                    value = {travel.nome}
+                    placeholder= 'Nome da viagem'
+                    onChange = {(e)=>setTravel({...travel,nome: e.target.value})}
+                    />
+                    <label >Descrição</label>
+                    <textarea
+                    required
+                    id = "desc"
+                    onChange = {(e)=>setTravel({...travel,desc: e.target.value})}
+                    value={travel.desc}
+                    placeholder = 'Descrição da viagem'
+                    />
+                    <label >Data da viagem</label>
+                    <input
+                    required
+                    type = 'datetime-local'
+                    id = "data"
+                    value={travel.data}
+                    placeholder = 'Data'
+                    onChange = {(e)=>setTravel({...travel,data: e.target.value})}
+                    />
+                    <label >Preço</label>
+                    <input
+                    required
+                    id = 'price'
+                    value = {travel.price}
+                    placeholder = 'Preço'
+                    type = 'number'
+                    onChange = {(e)=>setTravel({...travel,price: e.target.value})}
+                    />
+                    <button>Cadastrar Viagem</button>
+                </form>
+            </div>
         </>
     )
 }
