8eb4aa0a52d309d6bd6050f55b528d70cf22e820
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-05-06 08:53:46 -0300

    chamando o form para um componente

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import Form from './components/Form'
 function App() {
   const [travels,setTravels] = useState([]);
   const [travel, setTravel] = useState({nome:'', data:'', price:0, desc:''})
@@ -26,38 +27,11 @@ function App() {
   }
   return (
     <div className="App">
-      <form onSubmit={EnvioFormulario}>
-        <input
-          id ='nome'
-          required
-          value = {travel.nome}
-          placeholder= 'Nome da viagem'
-          onChange = {(e)=>setTravel({...travel,nome: e.target.value})}
-        />
-        <textarea
-          required
-          id = "desc"
-          onChange = {(e)=>setTravel({...travel,desc: e.target.value})}
-          value={travel.desc}
-          placeholder = 'Descrição da viagem'
-        />
-        <input
-          requiredtype = 'datetime-local'
-          id = "data"
-          value={travel.data}
-          placeholder = 'Data'
-          onChange = {(e)=>setTravel({...travel,data: e.target.value})}
-        />
-        <input
-          required
-          id = 'price'
-          value = {travel.price}
-          placeholder = 'Preço'
-          type = 'number'
-          onChange = {(e)=>setTravel({...travel,price: e.target.value})}
-        />
-        <button>Cadastrar Viagem</button>
-      </form>
+      <Form
+        travel = {travel}
+        setTravel = {setTravel}
+        EnvioFormulario = {EnvioFormulario}
+      />
       {
         travels.map(viagem=><h1 key={viagem.id}>{viagem.nome}</h1>)
       }
