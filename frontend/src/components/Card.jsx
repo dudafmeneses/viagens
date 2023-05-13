908c9a72791a59b147d49c8b1d6c8970888ea681
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-05-13 09:39:11 -0300

    animação de delete

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,19 +1,28 @@
 import "./Card.css"
 import {FiTrash2} from 'react-icons/fi'
+import { FormatarData } from "../fn-helpers/Data"
+import { useState } from "react";
 function Card(props){
     const {id,data,desc,price} = props
+    const [isDelete, setIsDelete] = useState(false)
+    const deleteMode=()=>{
+        setIsDelete(true);
+        setTimeout(()=>{
+            props.deleteTravel(id)
+        }, 700)
+    }
     return (
         <>
-            <div className = "Card">
+            <div className ={isDelete? "Card disappear": "Card"}>
                 <h1>{props.nome}</h1>
                 <div className = "conteudoCard">
                     <p>{desc}</p>
-                    <p>{data}</p>
+                    <p>{FormatarData(data)}</p>
                     <p>{price}</p>
                 </div>
                 <div className = "bottom">
                     <div className = "botoes">
-                        <div onClick={()=>props.deleteTravel(id)}>
+                        <div onClick={()=>deleteMode()}>
                         <FiTrash2 size={28}/>
                         </div>
                         <div className = "icons">
--- frontend/src/fn-helpers/Data.js
+++ frontend/src/fn-helpers/Data.js
@@ -3,8 +3,8 @@ export function FormatarData(data){
     return brasilDateTiming
 }
 
-function formatDateTime(){
-    const dataObj = new Date();
+function formatDateTime(data){
+    const dataObj = new Date(data);
     const day = dataObj.getUTCDate().toString().padStart(2, "0");
     const month = (dataObj.getUTCMonth()+1).toString().padStart(2,"0");
     const year = dataObj.getUTCFullYear().toString();
