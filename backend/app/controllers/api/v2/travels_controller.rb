bc3cfb168f2bbc1b8b69c2088835937740e25cb1
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-05-27 09:24:12 -0300

    controller clonado

--- backend/app/controllers/api/v2/travels_controller.rb
+++ backend/app/controllers/api/v2/travels_controller.rb
@@ -1,16 +1,37 @@
 class Api::V2::TravelsController < ApplicationController
   def index
+    @travels = Travel.all
+    render json: @travels
   end
 
   def show
   end
 
-  def update
+  def create
+    @travel = Travel.new(travel_params)
+    if @travel.save
+      render json: @travel
+    else
+      render json: {errors: @travel.errors, message: "o cadastro deu errado"}, status: :unprocessable_entity
+    end
   end
 
-  def create
+  def update
+    @travel = Travel.find(params[:id])
+    if @travel.update(travel_params)
+      render json: @travel
+    else
+      render json: {errors: @travel.errors, message: "a edição deu errado"}, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @travel = Travel.find(params[:id])
+    @travel.destroy
+    render json:{message: "Sua mensagem foi deletada com sucesso!"}
+  end
+  private
+  def travel_params
+    params.require(:travel).permit(:nome,:data,:price,:desc)
   end
 end
